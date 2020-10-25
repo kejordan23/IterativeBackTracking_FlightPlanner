66488d2aa7682e401f990d3103e5b2ebc50967ab
Kylie <kejordan@mail.smu.edu>
2020-10-25 11:14:46 -0500

    adding list class and DSString

--- main.cpp
+++ main.cpp
@@ -5,8 +5,12 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
     ofstream output;
+    ifstream input1;
+    ifstream input2;
+    input1.open(argv[1]);
+    input2.open(argv[2]);
     output.open(argv[3]);
-    if (!output.is_open())
+    if (!output.is_open() || !input1.is_open() || !input2.is_open())
         cout << "unable to open file" << endl;
     output<<"Hello World!"<<endl;
     return 0;
